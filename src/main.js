a702de3097494a438069eead9f72208895173397
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2019-10-28 02:12:12 -0600

    configuracion de inicio test srenvio

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Topbar from "./components/Topbar.vue";
+import VueRouter from "vue-router";
+import {
+  MdButton,
+  MdContent,
+  MdTabs,
+  MdList
+} from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
+Vue.use(MdList);
+Vue.config.productionTip = false;
+
+const linkActiveClass = "my-link-active-class";
+
+// pass custom class to Vue Material
+Vue.material.router.linkActiveClass = linkActiveClass;
+
+const routes = [
+  {
+    path: "/",
+    name: "TabRouter",
+    component: { template: Topbar }
+  }
+];
+// pass custom class
+const router = new VueRouter({
+  routes // short for `routes: routes`,
+  // linkActiveClass
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router
+}).$mount("#app");
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  css: {
+    requireModuleExtension: true,
+    sourceMap: true
+  },
+
+  runtimeCompiler: true
+}
